48d34f659a063f4ef73839bf16a903dcc7d9446a
Akihiro Douke <douke@example.com>
2026-02-13 15:36:45 +0900

    fix(deployment): use serverless function to serve ads.txt reliably

---
+++ api/ads.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send('google.com, pub-3080048643511723, DIRECT, f08c47fec0942fa0');
+}
